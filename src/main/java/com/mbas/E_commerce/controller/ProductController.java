aeecd26dcfe44196ef580b0798f63f6b16725e2b
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-11-14 06:14:26 +0100

    Filter by category name

--- src/main/java/com/mbas/E_commerce/controller/ProductController.java
+++ src/main/java/com/mbas/E_commerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.mbas.E_commerce.dto.ProductDto;
 import com.mbas.E_commerce.entities.Product;
 import com.mbas.E_commerce.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -37,14 +37,33 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        var product = productRepository.findById(id).orElse(null);
-
-        return ResponseEntity.ok(product);
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(product -> new ProductDto(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice(),
+                        product.getCategory().getName()
+                ))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-//    @GetMapping("/category/{categoryId}")
-//    public List<Product> getProductsByCategory(@PathVariable Byte categoryId) {
-//        return productRepository.findByCategoryId(categoryId);
-//    }
+    // Filter by category name
+    @GetMapping("/category-name/{categoryName}")
+    public List<ProductDto> getProductsByCategoryName(@PathVariable String categoryName) {
+        List<Product> products = productRepository.findByCategoryName(categoryName);
+
+        return products.stream()
+                .map(product -> new ProductDto(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice(),
+                        product.getCategory().getName()
+                ))
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/mbas/E_commerce/repository/ProductRepository.java
+++ src/main/java/com/mbas/E_commerce/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.mbas.E_commerce.repository;
 
+import com.mbas.E_commerce.dto.ProductDto;
 import com.mbas.E_commerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryId(Byte categoryId);
+    List<Product> findByCategoryName(String name);
 }
