96f4f5bec1c41d61e37556e2521dba7c3f46a8e5
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-12-24 18:20:13 +0100

    write basic config for spring security

--- src/main/java/com/mbas/E_commerce/config/SecurityConfig.java
+++ src/main/java/com/mbas/E_commerce/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +14,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Stateless Sessions
+        // Stateless Sessions (token-based authentication)
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(session ->
+                        session.anyRequest().permitAll()
+                );
+        return http.build();
         // Disable CSRF
         // Authorize
-
-        return null;
     }
 }
