a23133ebe5aec58c3a788e049006d6e3d80b4b06
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-12-28 14:53:47 +0100

    implement jwts

--- src/main/java/com/mbas/E_commerce/controller/AuthController.java
+++ src/main/java/com/mbas/E_commerce/controller/AuthController.java
@@ -1,30 +1,40 @@
 package com.mbas.E_commerce.controller;
 
+import com.mbas.E_commerce.dto.JwtResponse;
 import com.mbas.E_commerce.dto.LoginRequest;
+import com.mbas.E_commerce.service.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()
                 )
         );
-        return ResponseEntity.ok().build();
+
+        var token = jwtService.generateToken(request.getEmail());
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
---
+++ src/main/java/com/mbas/E_commerce/dto/JwtResponse.java
@@ -0,0 +1,10 @@
+package com.mbas.E_commerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+}
